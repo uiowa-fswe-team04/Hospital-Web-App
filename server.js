e8f711f1de3af92f3ef8d25992e7b93db2463724
Evan Bradley <evan-bradley@uiowa.edu>
2021-10-27 21:21:43 -0500

    connected to mysql

--- server.js
+++ server.js
@@ -1,5 +1,53 @@
 const express = require("express");
+const mysql = require('mysql');
 const app = express();
+
+// connection with DB
+const db = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '123456',
+  insecureAuth : true,
+  database : 'hospitalapp'
+})
+
+db.connect((err) => {
+    if(err){
+      throw err;
+    }
+    console.log("Connected to MYSQL")
+});
+
+// Create DB
+app.get('/createdb', (req, res) => {
+  let sql = 'CREATE DATABASE hospitalapp';
+  db.query(sql, (err, result) =>{
+    if(err) throw err;
+    console.log("DATABASE CREATED")
+    console.log(result);
+  });
+});
+
+// Create table
+app.get('/createposttable', (req, res) => {
+  let sql = 'CREATE TABLE post(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY (id))';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send("POSTS TABLE created");
+  });
+});
+
+// Insert post 1
+app.get('/addpost1', (req,res) => {
+  let post = {title: 'Post 1', body:'this is an example'}
+  let sql = "INSERT INTO post SET ?";
+  let query = db.query(sql, post, (err, result) =>{
+    if (err) throw err;
+    console.log(result);
+    res.send("Post 1 added")
+  });
+});
 
 const { auth } = require('express-openid-connect');
 const config = {
