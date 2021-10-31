31781b8006468030d9a8be1cfb92d8de43ec7654
Hogan <hoggiesub1234@gmail.com>
2021-10-31 14:58:33 -0500

    basics of RBAC

--- auth/config_Auth0.js
+++
@@ -1,2 +0,0 @@
-let callback_url = "http://localhost:3000/callback";
-let logout_url = "http://localhost:3000";
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   user : 'root',
   password : '123456',
   insecureAuth : true,
-  database : 'hospitalapp'
+  database : "hospitalapp"
 })
 
 db.connect((err) => {
@@ -49,6 +49,7 @@ app.get('/addpost1', (req,res) => {
   });
 });
 
+// Authentication and protected routes
 const { auth } = require('express-openid-connect');
 const config = {
   authRequired: false,
@@ -73,7 +74,26 @@ const listener = app.listen(process.env.PORT || 80, () => {
 app.get("/", function (request, response) {
   if (request.oidc.isAuthenticated())
   {
-    response.sendFile(__dirname + '/private/landing_page.html');
+
+    get_user_role(request.oidc.user["email"], function(err, user_role) {
+      if (user_role == []){
+        add_user_role(request.oidc.user["email"], "patient");
+        user_role = "patient";
+      } else {
+        user_role = user_role[0].role;
+      }
+  
+      if (user_role == "practitioner") {
+        response.sendFile(__dirname + '/private/doctor/landing_page.html');
+      } else if (user_role == "admin") {
+        response.sendFile(__dirname + '/private/admin/landing_page.html');
+      } else if (user_role == "patient") {
+        response.sendFile(__dirname + '/private/patient/landing_page.html');
+      } else {
+        response.redirect('/logout');
+      }
+    });
+    
   }
   else
   {
@@ -84,3 +104,34 @@ app.get("/", function (request, response) {
 app.get('/profile', requiresAuth(), (request, response) => {
   response.send(JSON.stringify(request.oidc.user));
 });
+
+// Create roles table
+app.get('/createuserroletable', (req, res) => {
+  let sql = 'CREATE TABLE user_roles(email VARCHAR(255), role VARCHAR(255))';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send("Roles table created!");
+  });
+});
+
+function add_user_role(user_email, user_role)
+{
+  let post = {email: user_email, role: user_role}
+  let sql = "INSERT INTO user_roles SET ?";
+  let query = db.query(sql, post, (err, result) =>{
+    if (err) throw err;
+    console.log("Role added!");
+  });
+}
+async function get_user_role(user_email, callback)
+{
+  user_email = "test_user@test.com";
+  let sql = "SELECT role FROM user_roles WHERE email = ?";
+  let post = user_email;
+  db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    callback (null, result);
+  });
+  
+}
