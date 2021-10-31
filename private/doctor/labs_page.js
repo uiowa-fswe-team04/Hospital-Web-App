3f75aabfe2fa2b784b8b02a0794877757bf376e7
Evan Bradley <evan-bradley@uiowa.edu>
2021-10-31 18:26:12 -0500

    super buggy but login goes to correct landing page

---
+++ private/doctor/labs_page.js
@@ -0,0 +1,15 @@
+document.getElementById('home_bar').addEventListener('click', function(){
+    window.location.replace("landing_page.html")
+});
+
+document.getElementById('patient_bar').addEventListener('click', function(){
+  window.location.replace("patients_page.html")
+});
+
+document.getElementById('labs_bar').addEventListener('click', function(){
+  window.location.replace("labs_page.html")
+});
+
+document.getElementById('notification_bar').addEventListener('click', function(){
+  window.location.replace("notifications_page.html")
+});
--- private/patient/patients_page.js
+++ private/patient/patients_page.js
@@ -7,17 +7,17 @@ document.getElementById('sign_out_button').addEventListener('click', function() 
 });
 
 document.getElementById('home_bar').addEventListener('click', function(){
-    window.location.replace("private/patient/landing_page.html")
+    window.location.replace("landing_page.html")
 });
 
 document.getElementById('patient_bar').addEventListener('click', function(){
-  window.location.replace("private/patient/patients_page.html")
+  window.location.replace("patients_page.html")
 });
 
 document.getElementById('labs_bar').addEventListener('click', function(){
-  window.location.replace("private/patient/labs_page.html")
+  window.location.replace("labs_page.html")
 });
 
 document.getElementById('notification_bar').addEventListener('click', function(){
-  window.location.replace("private/patient/notifications_page.html")
+  window.location.replace("notifications_page.html")
 });
