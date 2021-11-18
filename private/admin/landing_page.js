7feb19ad4766659db69ef741d8ce2c1147ca253e
Hogan <hoggiesub1234@gmail.com>
2021-11-18 12:39:23 -0600

    finishes touches on doctor creation use case

--- private/admin/landing_page.js
+++ private/admin/landing_page.js
@@ -11,5 +11,5 @@ document.getElementById('home_bar').addEventListener('click', function(){
 });
 
 document.getElementById('doctor_bar').addEventListener('click', function(){
-  window.location.href =("/private/admin/doctors_page.html")
+  window.location.href =("/private/admin/doctor_management/dm_landing.html")
 });
