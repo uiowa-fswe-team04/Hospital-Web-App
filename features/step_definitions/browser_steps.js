06b3d1965dd7670b783de0e7af5ec6f2de055b50
Evan Bradley <evan-bradley@uiowa.edu>
2021-11-29 21:13:51 -0600

    Added boilerplate code for testing

---
+++ features/step_definitions/browser_steps.js
@@ -0,0 +1,15 @@
+var seleniumWebdriver = require('selenium-webdriver');
+var {Given, Then, When} = require('cucumber');
+
+  Given('This is a test', function() {
+    return 'pending';
+  });
+
+  When('I click on {stringInDoubleQuotes}', function (text) {
+    return 'pending';
+  });
+
+  Then('I should see {stringInDoubleQuotes}', function (text) {
+    return 'pending';
+  });
+
