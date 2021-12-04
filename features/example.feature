12395d21865c76d7383d7ab3fc19b77b79a9fefb
Evan Bradley <evan-bradley@uiowa.edu>
2021-12-03 21:55:34 -0600

    testing updates

--- features/example.feature
+++ features/example.feature
@@ -4,6 +4,6 @@ Feature: Example feature
   So that I can concentrate on building awesome applications
 
   Scenario: Reading documentation
-    Given I am on the Cucumber.js GitHub repository
-    When I click on "CLI"
-    Then I should see "Running specific features"
+    Given This is a test
+    When I click on test
+    #Then I should see test
--- features/step_definitions/browser_steps.js
+++ features/step_definitions/browser_steps.js
@@ -1,15 +1,23 @@
 var seleniumWebdriver = require('selenium-webdriver');
+const { Builder, By, Capabilities, Key, until } = require('selenium-webdriver');
 var {Given, Then, When} = require('cucumber');
 
-  Given('This is a test', function() {
-    return 'pending';
+  Given('This is a test', async function() {
+    this.driver = new Builder()
+        .forBrowser('chrome')
+        .build();
+ 
+    this.driver.wait(until.elementLocated(By.tagName('h1')));
+ 
+    await this.driver.get('http://localhost:80');
+    return true;
   });
 
-  When('I click on {stringInDoubleQuotes}', function (text) {
-    return 'pending';
+  When('I click on test', function () {
+    return true;
   });
 
-  Then('I should see {stringInDoubleQuotes}', function (text) {
-    return 'pending';
+  Then('I should see test', function () {
+    return true;
   });
 
