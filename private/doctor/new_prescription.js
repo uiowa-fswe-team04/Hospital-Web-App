1c8f4f6c6ab2943bcac2237d2b8dca996bc3c8a0
Hogan <hoggiesub1234@gmail.com>
2021-12-06 23:00:46 -0600

    dr can only see patients that have appointments with them

--- private/doctor/new_prescription.js
+++ private/doctor/new_prescription.js
@@ -22,35 +22,55 @@ document.getElementById('create_prescription_button').addEventListener('click', 
 });  
 
 function getPatients() 
-{ 
-    // makes GET request to populate drop down menu
+{
+  // Get user name
+  var cookie_to_use = "";
+  var cookie_name = "email=";
+  var raw_cookie = decodeURIComponent(document.cookie);
+  var cookie_arr = raw_cookie.split(";");
+  for (var i = 0; i < cookie_arr.length; i++)
+    {
+      // Checks if begins with space and removes
+      if (cookie_arr[i][0] == " ") {
+        cookie_arr[i] = cookie_arr[i].substring(1);
+      }
+      if (cookie_arr[i].substring(0, cookie_name.length) == cookie_name)
+      {
+        cookie_to_use = cookie_arr[i].substring(cookie_name.length, cookie_arr[i].length);
+        break;
+      }
+    }
+
+  // makes GET request to populate drop down menu
   var host = location.hostname;
-  var url = 'http://' + host + '/request_patients';
+  var url = 'http://' + host + '/get_appointments_list';
   
   // http get request
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-          try {
-            var result_arr = JSON.parse(xmlHttp.responseText);
-          } catch (err) {
-            // throw an error
-           console.log(err);
-          }   
-          
-          select = document.getElementById('patients');
-          for (const pat in result_arr){
-            var name = result_arr[pat]['name']
-            var opt = document.createElement('option');
-            opt.value = name;
-            opt.innerHTML = name;
-            select.appendChild(opt);
-            
-            //var container = document.createElement("div");
-            //container.innerHTML = html_options;
-            //document.getElementById('hunt_list').appendChild(container);
-            //huntList.push(result_arr[hunt]);
-          }
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        try {
+          var result_arr = JSON.parse(xmlHttp.responseText);
+        } catch (err) {
+          // throw an error
+          console.log(err);
+        }   
+        
+        console.log(result_arr);
+
+        select = document.getElementById('patients');
+        if (result_arr){
+          for (const appointment of Object.keys(result_arr)){
+            if (result_arr[appointment]["name"] == cookie_to_use)
+            {
+              var patient_name = result_arr[appointment]['patient']
+              var opt = document.createElement('option');
+              opt.value = patient_name;
+              opt.innerHTML = patient_name;
+              select.appendChild(opt);
+            }
+          };
+        }
   }
   xmlHttp.open("GET", url, true);
   xmlHttp.send(null);
