3b901ce5dbeb1f84f3ae94476f4970c59c315b5a
Evan Bradley <evan-bradley@uiowa.edu>
2021-12-06 21:33:12 -0600

    patient prescription css

--- private/patient/notifications_page.js
+++ private/patient/notifications_page.js
@@ -68,8 +68,7 @@ break;
             var cell1 = row.insertCell(0);
             var cell2 = row.insertCell(1);
             var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-
+      
             cell1.innerHTML = patients[i]["name"];
             cell2.innerHTML = patients[i]["medication"];
             cell3.innerHTML = patients[i]["notes"];
