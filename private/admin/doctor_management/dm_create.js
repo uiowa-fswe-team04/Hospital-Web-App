fdb8aead82d3bb30503f743b369c9cd99067cc41
Hogan <hoggiesub1234@gmail.com>
2021-12-06 22:24:39 -0600

    ahhh

--- private/admin/doctor_management/dm_create.js
+++ private/admin/doctor_management/dm_create.js
@@ -3,17 +3,23 @@ document.getElementById('create_account_button').addEventListener('click', funct
     var email = document.getElementById('inputEmail').value;
     var password = document.getElementById('inputPasswordOrig').value;
     var password_confirm = document.getElementById('inputPasswordConfirm').value;
+    var name = document.getElementById('inputName').value;
   
     if (password != password_confirm) {
         alert("Passwords must match");
         return;
     }
+
+    let login = new FormData();
+    login.append("email", email);
+    login.append("password", password);
+    login.append("name", name);
     
     const XML_req_hunt = new XMLHttpRequest();
     // successful data submission
     XML_req_hunt.addEventListener('load', function( event ) {
         alert('Account created');
-        window.location.replace('http://' + host);
+        window.location.replace('http://' + host + '/private/admin/doctor_management/dm_landing.html');
     });
     // error
     XML_req_hunt.addEventListener(' error', function( event ) {
@@ -21,7 +27,7 @@ document.getElementById('create_account_button').addEventListener('click', funct
     });
     // set up POST request
     var host = location.hostname;
-    var url = 'http://' + host + '/create_doctor_user?email=' + email;
-    XML_req_hunt.open('GET', url);
-    XML_req_hunt.send(null);
+    var url = 'http://' + host + '/create_doctor_user';
+    XML_req_hunt.open('POST', url);
+    XML_req_hunt.send(login);
 });  
