f605f6f6e202eb5b6ab3cf2f1fd754a4b60828d6
Hogan <hoggiesub1234@gmail.com>
2021-12-06 20:52:06 -0600

    sign out fix 2

--- private/admin/doctor_management/dm_landing.js
+++ private/admin/doctor_management/dm_landing.js
@@ -5,8 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('sign_out_button').addEventListener('click', function() {
       // redirect to logout path
       var host = location.hostname;
-      var logout_route = 'http://' + host + '/logout';
-      document.location.replace(logout_route);
+      document.cookie = "email=expireme;path=/;expires=Tue, 14 Aug 1945 12:00:00 UTC";
+      document.cookie = "password=expireme;path=/;expires=Tue, 14 Aug 1945 12:00:00 UTC";
+      document.location.replace('http://' + host + '/public/auth/login.html');
     });
     
     document.getElementById('home_bar').addEventListener('click', function(){
@@ -16,14 +17,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('doctor_bar').addEventListener('click', function(){
       window.location.href =("/private/admin/doctor_management/dm_landing.html")
     });
-});
-
-document.getElementById('sign_out_button').addEventListener('click', function() {
-  // redirect to logout path
-  var host = location.hostname;
-  document.cookie = "email=expireme;path=/;expires=Tue, 14 Aug 1945 12:00:00 UTC";
-  document.cookie = "password=expireme;path=/;expires=Tue, 14 Aug 1945 12:00:00 UTC";
-  document.location.replace('http://' + host + '/public/auth/login.html');
 });
 
 function populate_doctors() {
